b91d313d5aed4fa749f99ce2b6b21cbbfa6e68d7
A Venkata Sai Krishna Varun <varunias2004@gmail.com>
2024-05-28 02:22:20 +0530

    Update test.py

--- cloud-medbot-main/test.py
+++ cloud-medbot-main/test.py
@@ -56,9 +56,10 @@ def get_response(user_input):
     result = model.predict(padded_sequence)
     tag = lbl_encoder.inverse_transform([np.argmax(result)])[0]
     probability = np.max(result)
-    print(i['tags'])
     for i in data['intents']:
+        print(i['tags'])
         if i['tags'][0] == tag:
+            print("yes")
             is_available=True
             break
         else:
